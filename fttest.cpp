7999ac6b1af67e5d4008b05495ca3fdce681fa11
Jussi Pakkanen <jpakkane@gmail.com>
2022-03-18 21:05:45 +0200

    Stuff.

--- fttest.cpp
+++ fttest.cpp
@@ -1,6 +1,8 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+#include <cairo-pdf.h>
+
 #include <string>
 
 #include <cstdlib>
@@ -19,25 +21,40 @@ void fterror(FT_Error ec) {
 template<typename T> double ftmetric2mm(T v) { return v / 300 * 25.4 / 64; }
 double glyph2mm(FT_Face face) { return ftmetric2mm(face->glyph->advance.x); }
 
+void draw_letter(cairo_t *cr, char l, double xoffset) {
+    char tmp[2] = {l, 0};
+    cairo_save(cr);
+    cairo_move_to(cr, 50 + 1.15 * xoffset / 220 * 595, 100);
+    cairo_show_text(cr, tmp);
+    cairo_restore(cr);
+}
+
 int main() {
+    const int FONTSIZE = 12;
     FT_Library library;
     FT_Error ec;
     FT_Face face;
+    cairo_surface_t *surf = cairo_pdf_surface_create("cairotest.pdf", 595, 842);
+    cairo_t *cr = cairo_create(surf);
+    cairo_select_font_face(cr, "Gentium", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size(cr, FONTSIZE);
     ec = FT_Init_FreeType(&library);
     CHECKFT(ec);
-    ec = FT_New_Face(
-        library, "/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf", 0, &face);
+    ec = FT_New_Face(library, "/usr/share/fonts/truetype/gentium/Gentium-R.ttf", 0, &face);
     CHECKFT(ec);
     printf("Font has kerning %d.\n", FT_HAS_KERNING(face));
-    ec = FT_Set_Char_Size(face, 0, 12 * 64, 300, 300);
+    ec = FT_Set_Char_Size(face, 0, FONTSIZE * 64, 300, 300);
     CHECKFT(ec);
-    const std::string testword{"Maapallon keskushallinto Omniviraatin avaruusvoimien"};
+    const std::string testword{
+        "kenoiva ja jonka vetolujuus oli terasvaijerin tasoa. Kestavyys tuli "
+        "tarpeeseen, silla kyseinen"};
     std::string_view latter{testword};
     latter.remove_prefix(1);
     auto glyph_index = FT_Get_Char_Index(face, testword[0]);
     assert(glyph_index != 0);
     ec = FT_Load_Glyph(face, glyph_index, FT_LOAD_TARGET_MONO);
     CHECKFT(ec);
+    draw_letter(cr, testword[0], 0);
     double total_w = glyph2mm(face);
     auto previous_index = glyph_index;
     FT_Vector v;
@@ -45,7 +62,11 @@ int main() {
         glyph_index = FT_Get_Char_Index(face, c);
         ec = FT_Get_Kerning(face, previous_index, glyph_index, FT_KERNING_DEFAULT, &v);
         CHECKFT(ec);
-        total_w += ftmetric2mm(v.x);
+        if(v.x != 0) {
+            printf("Non-zero kerning pair.\n");
+            total_w += ftmetric2mm(v.x);
+        }
+        draw_letter(cr, c, total_w);
         ec = FT_Load_Glyph(face, glyph_index, FT_LOAD_TARGET_MONO);
         CHECKFT(ec);
         total_w += glyph2mm(face);
@@ -56,4 +77,8 @@ int main() {
     CHECKFT(ec);
     FT_Done_FreeType(library);
     CHECKFT(ec);
+
+    cairo_move_to(cr, 100, 100);
+    cairo_surface_destroy(surf);
+    cairo_destroy(cr);
 }
--- pangotest.cpp
+++ pangotest.cpp
@@ -1,22 +1,32 @@
 #include <pango/pangocairo.h>
+#include <cairo-pdf.h>
 
 #include <clocale>
 #include <cassert>
 
 int main() {
     cairo_status_t status;
-    cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 1024, 1024);
+    cairo_surface_t *surface = cairo_pdf_surface_create("pangocairotest.pdf", 595, 842);
     cairo_t *cr = cairo_create(surface);
+    cairo_move_to(cr, 72, 72);
     setlocale(LC_ALL, "");
     printf("PANGO_SCALE = %d\n", PANGO_SCALE);
     PangoLayout *layout = pango_cairo_create_layout(cr);
     PangoFontDescription *desc;
-    desc = pango_font_description_from_string("Liberation Serif,Serif");
+    desc = pango_font_description_from_string("Gentium");
     assert(desc);
     pango_font_description_set_absolute_size(desc, 12 * PANGO_SCALE);
     pango_layout_set_font_description(layout, desc);
     pango_font_description_free(desc);
-    pango_layout_set_text(layout, "Maapallon keskushallinto Omniviraatin avaruusvoimien.", -1);
+    pango_layout_set_text(layout,
+                          "kenoiva ja jonka vetolujuus oli terasvaijerin tasoa. Kestavyys tuli "
+                          "tarpeeseen, silla kyseinen",
+                          -1);
+    auto liter = pango_layout_get_iter(layout);
+    PangoRectangle ink_rect, logical_rect;
+    pango_layout_iter_get_line_extents(liter, &ink_rect, &logical_rect);
+    pango_layout_iter_free(liter);
+    printf("Text width is %.2f mm\n", double(logical_rect.width) / PANGO_SCALE / 595 * 220);
     pango_cairo_update_layout(cr, layout);
     pango_cairo_show_layout(cr, layout);
     int w, h;
@@ -24,8 +34,6 @@ int main() {
     printf("Size: (%d, %d)\n", w / PANGO_SCALE, h / PANGO_SCALE);
     g_object_unref(G_OBJECT(layout));
     //    g_object_unref(G_OBJECT(context));
-    status = cairo_surface_write_to_png(surface, "cairotest.png");
-    assert(status == 0);
     cairo_surface_destroy(surface);
     cairo_destroy(cr);
     return 0;
