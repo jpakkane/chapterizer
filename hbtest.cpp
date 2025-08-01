fa542d552c3ff5d0ba71fc9934e3c8b31afd9a70
Jussi Pakkanen <jussi.pakkanen@mailbox.org>
2025-08-01 15:41:52 +0300

    Compute widths for real.

--- hbtest.cpp
+++ hbtest.cpp
@@ -2,12 +2,17 @@
 // Copyright 2025 Jussi Pakkanen
 
 #include <hb.h>
+#include <cassert>
 
 #include <filesystem>
 #include <memory>
 
 struct HBFontCloser {
     void operator()(hb_font_t *f) const noexcept { hb_font_destroy(f); }
+};
+
+struct HBBufferCloser {
+    void operator()(hb_buffer_t *b) const noexcept { hb_buffer_destroy(b); }
 };
 
 struct FontFiles {
@@ -139,14 +144,83 @@ hb_font_t *FontCache::get_font(TextCategory cat, TextStyle style) {
     std::abort();
 }
 
-int width_test(
-    FontCache &fc, TextStyle style, TextExtra extra, double pointsize, const char *u8text) {
-    return pointsize;
+double width_test(FontCache &fc,
+                  TextCategory cat,
+                  TextStyle style,
+                  TextExtra extra,
+                  double pointsize,
+                  const char *u8text) {
+    double total_width = 0;
+    hb_buffer_t *buf;
+    const double num_steps = 64;
+    const double hbscale = pointsize * num_steps;
+    buf = hb_buffer_create();
+    assert(buf);
+    std::unique_ptr<hb_buffer_t, HBBufferCloser> bc(buf);
+    hb_buffer_add_utf8(buf, u8text, -1, 0, -1);
+    hb_buffer_set_direction(buf, HB_DIRECTION_LTR);
+    hb_buffer_set_script(buf, HB_SCRIPT_LATIN);
+    hb_buffer_set_language(buf, hb_language_from_string("en", -1));
+
+    hb_buffer_guess_segment_properties(buf);
+
+    hb_font_t *font = fc.get_font(cat, style);
+    if(!font) {
+        printf("Requested font does not exist.\n");
+        std::abort();
+    }
+    hb_font_set_scale(font, hbscale, hbscale);
+
+    if(extra == TextExtra::None) {
+        hb_shape(font, buf, nullptr, 0);
+    } else if(extra == TextExtra::SmallCaps) {
+        hb_feature_t userfeatures[1];
+        userfeatures[0].tag = HB_TAG('s', 'm', 'c', 'p');
+        userfeatures[0].value = 1;
+        userfeatures[0].start = HB_FEATURE_GLOBAL_START;
+        userfeatures[0].end = HB_FEATURE_GLOBAL_END;
+        hb_shape(font, buf, userfeatures, 1);
+    } else {
+        std::abort();
+    }
+
+    unsigned int glyph_count;
+    hb_glyph_info_t *glyph_info = hb_buffer_get_glyph_infos(buf, &glyph_count);
+    if(!glyph_info) {
+        printf("Could not get glyph info.\n");
+        std::abort();
+    }
+    hb_glyph_position_t *glyph_pos = hb_buffer_get_glyph_positions(buf, &glyph_count);
+    if(!glyph_pos) {
+        printf("Could not get glyph pos.\n");
+        std::abort();
+    }
+
+    for(unsigned int i = 0; i < glyph_count; i++) {
+        // const hb_glyph_info_t *current = glyph_info + i;
+        const hb_glyph_position_t *curpos = glyph_pos + i;
+        const auto advance = curpos->x_advance / hbscale;
+        total_width += advance;
+    }
+
+    return total_width * pointsize;
 }
 
 int main(int, char **) {
     FontCache fc;
-    const int text_width = width_test(fc, TextStyle::Regular, TextExtra::None, 12, "abc");
-    printf("Width of text is: %d\n", text_width);
+    const char *text = "Hello world!";
+    const double ptsize = 10;
+    double text_width =
+        width_test(fc, TextCategory::Serif, TextStyle::Regular, TextExtra::None, ptsize, text);
+    printf("Width of text is: %.2f\n", text_width);
+    text_width =
+        width_test(fc, TextCategory::Serif, TextStyle::Italic, TextExtra::None, ptsize, text);
+    printf("Width of italic text is: %.2f\n", text_width);
+    text_width =
+        width_test(fc, TextCategory::Serif, TextStyle::Regular, TextExtra::SmallCaps, ptsize, text);
+    printf("Width of smallcaps text is: %.2f\n", text_width);
+    text_width =
+        width_test(fc, TextCategory::Serif, TextStyle::Regular, TextExtra::None, ptsize, text);
+    printf("Width of text is: %.2f\n", text_width);
     return 0;
 }
